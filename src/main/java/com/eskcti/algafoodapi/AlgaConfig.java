b5eeefda52b0b5411b891c1f8f7d6beb501b1b36
eskokado <eskokado@gmail.com>
2023-05-11 08:09:12 -0300

    feat: comment example @Bean and add Bean class String

--- src/main/java/com/eskcti/algafoodapi/AlgaConfig.java
+++ src/main/java/com/eskcti/algafoodapi/AlgaConfig.java
@@ -5,18 +5,22 @@ import com.eskcti.algafoodapi.di.service.ActivationCustomerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class AlgaConfig {
     @Bean
-    public NotifierEmail notifierEmail() {
-        System.out.println("NotifierEmail Bean");
-        NotifierEmail notifier = new NotifierEmail("smtp.algamail.com.br");
-        notifier.setCapsLock(true);
-        return notifier;
+    public String string() {
+        return new String();
     }
-
-    @Bean
-    public ActivationCustomerService activationCustomerService() {
-        return new ActivationCustomerService(notifierEmail());
-    }
+//    @Bean
+//    public NotifierEmail notifierEmail() {
+//        System.out.println("NotifierEmail Bean");
+//        NotifierEmail notifier = new NotifierEmail("smtp.algamail.com.br");
+//        notifier.setCapsLock(true);
+//        return notifier;
+//    }
+//
+//    @Bean
+//    public ActivationCustomerService activationCustomerService() {
+//        return new ActivationCustomerService(notifierEmail());
+//    }
 }

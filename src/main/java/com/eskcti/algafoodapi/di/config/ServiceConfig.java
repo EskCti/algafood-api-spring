5a96bfd8b892333d8a54e56361faa2e5f231c954
eskokado <eskokado@gmail.com>
2023-05-11 07:19:18 -0300

    feat: create ServiceConfig example using annotation @Configuration and @Bean

---
+++ src/main/java/com/eskcti/algafoodapi/di/config/ServiceConfig.java
@@ -0,0 +1,14 @@
+package com.eskcti.algafoodapi.di.config;
+
+import com.eskcti.algafoodapi.di.notification.Notifier;
+import com.eskcti.algafoodapi.di.service.ActivationCustomerService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+    @Bean
+    public ActivationCustomerService activationCustomerService(Notifier notifier) {
+        return new ActivationCustomerService(notifier);
+    }
+}

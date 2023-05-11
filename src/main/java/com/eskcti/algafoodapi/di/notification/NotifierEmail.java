e6b54bdcb18a6d1626ca07ad6ce104457f0e0e81
eskokado <eskokado@gmail.com>
2023-05-11 07:20:34 -0300

    feat: implement caps lock and host server smtp property in the class NotifierEmail

--- src/main/java/com/eskcti/algafoodapi/di/notification/NotifierEmail.java
+++ src/main/java/com/eskcti/algafoodapi/di/notification/NotifierEmail.java
@@ -3,15 +3,27 @@ package com.eskcti.algafoodapi.di.notification;
 import com.eskcti.algafoodapi.di.models.Customer;
 import org.springframework.stereotype.Component;
 
-@Component
 public class NotifierEmail implements Notifier {
-    public NotifierEmail() {
+
+    private boolean capsLock;
+    private String hostServerSmtp;
+
+    public NotifierEmail(String hostServerSmtp) {
+        this.hostServerSmtp = hostServerSmtp;
         System.out.println("NotifierEmail");
     }
 
     @Override
     public void notify(Customer customer, String message) {
-        System.out.printf("Notifying %s via email %s: %s\n",
-                customer.getName(), customer.getEmail(), message);
+        if (this.capsLock) {
+            message = message.toUpperCase();
+        }
+
+        System.out.printf("Notifying %s via email %s using SMTP %s: %s\n",
+                customer.getName(), customer.getEmail(), this.hostServerSmtp, message);
+    }
+
+    public void setCapsLock(boolean capsLock) {
+        this.capsLock = capsLock;
     }
 }

564ba0ccdf65151b0648e4557363789d545bafd8
eskokado <eskokado@gmail.com>
2023-05-12 08:41:29 -0300

    feat: comment class NotificationConfig

--- src/main/java/com/eskcti/algafoodapi/di/config/NotificationConfig.java
+++ src/main/java/com/eskcti/algafoodapi/di/config/NotificationConfig.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class NotificationConfig {
-    @Bean
-    public NotifierEmail notifierEmail() {
-        System.out.println("NotifierEmail Bean");
-        NotifierEmail notifier = new NotifierEmail("smtp.algamail.com.br");
-        notifier.setCapsLock(true);
-        return notifier;
-    }
+//    @Bean
+//    public NotifierEmail notifierEmail() {
+//        System.out.println("NotifierEmail Bean");
+//        NotifierEmail notifier = new NotifierEmail("smtp.algamail.com.br");
+//        notifier.setCapsLock(true);
+//        return notifier;
+//    }
 }

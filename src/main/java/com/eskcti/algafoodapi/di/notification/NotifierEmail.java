c2bbcf0e94d6c24b26554a73d0d87adf63e80d4d
eskokado <eskokado@gmail.com>
2023-05-12 08:41:04 -0300

    feat: add annotation @Profile class NotifierEmail

--- src/main/java/com/eskcti/algafoodapi/di/notification/NotifierEmail.java
+++ src/main/java/com/eskcti/algafoodapi/di/notification/NotifierEmail.java
@@ -4,10 +4,12 @@ import com.eskcti.algafoodapi.di.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Primary;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 //@Primary
 //@Qualifier("normal")
+@Profile("prod")
 @TypeOfNotifier(LevelNotifier.NORMAL)
 @Component
 public class NotifierEmail implements Notifier {
@@ -15,11 +17,15 @@ public class NotifierEmail implements Notifier {
     private boolean capsLock;
     private String hostServerSmtp;
 
-    @Autowired
-    public NotifierEmail(String hostServerSmtp) {
-        this.hostServerSmtp = hostServerSmtp;
-        System.out.println("NotifierEmail");
+    public NotifierEmail() {
+        System.out.println("Notificador Email REAL");
     }
+
+//    @Autowired
+//    public NotifierEmail(String hostServerSmtp) {
+//        this.hostServerSmtp = hostServerSmtp;
+//        System.out.println("NotifierEmail");
+//    }
 
     @Override
     public void notify(Customer customer, String message) {
@@ -27,8 +33,10 @@ public class NotifierEmail implements Notifier {
             message = message.toUpperCase();
         }
 
-        System.out.printf("Notifying %s via email %s using SMTP %s: %s\n",
-                customer.getName(), customer.getEmail(), this.hostServerSmtp, message);
+//        System.out.printf("Notifying %s via email %s using SMTP %s: %s\n",
+//                customer.getName(), customer.getEmail(), this.hostServerSmtp, message);
+        System.out.printf("Notifying %s via email %s: %s\n",
+                customer.getName(), customer.getEmail(), message);
     }
 
     public void setCapsLock(boolean capsLock) {

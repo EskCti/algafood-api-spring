023be6aa722dfe1e0ede80290695ae7d48fa826f
eskokado <eskokado@gmail.com>
2023-05-13 05:30:27 -0300

    feat: clear comment and add event publisher

--- src/main/java/com/eskcti/algafoodapi/di/service/ActivationCustomerService.java
+++ src/main/java/com/eskcti/algafoodapi/di/service/ActivationCustomerService.java
@@ -1,61 +1,19 @@
 package com.eskcti.algafoodapi.di.service;
 
 import com.eskcti.algafoodapi.di.models.Customer;
-import com.eskcti.algafoodapi.di.notification.LevelNotifier;
-import com.eskcti.algafoodapi.di.notification.Notifier;
-import com.eskcti.algafoodapi.di.notification.TypeOfNotifier;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-//@Component
+@Component
 public class ActivationCustomerService {
 
-//    @Qualifier("urgent")
-//    @TypeOfNotifier(LevelNotifier.URGENT)
-//    @Autowired(required = false)
-//    private List<Notifier> notifiers;
-    private Notifier notifier;
-
     @Autowired
-    public ActivationCustomerService(Notifier notifier) {
-        this.notifier = notifier;
-
-        System.out.println("ActivationCustomerService");
-    }
-//
-//    @Autowired
-//    public ActivationCustomerService() {
-//    }
-
-    public ActivationCustomerService(String qualquer) {
-
-    }
-
-//    @PostConstruct
-    public void init() {
-        System.out.println("INIT");
-    }
-
-//    @PreDestroy
-    public void destroy() {
-        System.out.println("DESTROY");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void activate(Customer customer) {
         customer.activate();
 
-//        for (Notifier notifier : notifiers) {
-            notifier.notify(customer, "Your registration in the system is active.");
-//        }
-//        if (notifier != null) {
-//            notifier.notify(customer, "Your registration in the system is active.");
-//        } else {
-//            System.out.println("Not notifier exists, but customer has been activated");
-//        }
+        eventPublisher.publishEvent(new CustomerActivatedEvent(customer));
     }
 }

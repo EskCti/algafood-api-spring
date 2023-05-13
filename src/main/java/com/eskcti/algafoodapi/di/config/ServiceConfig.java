f52567c6e308dbf4e0cdc01c3879bc823c8fea9b
eskokado <eskokado@gmail.com>
2023-05-13 05:28:18 -0300

    feat: comment class ServiceConfig

--- src/main/java/com/eskcti/algafoodapi/di/config/ServiceConfig.java
+++ src/main/java/com/eskcti/algafoodapi/di/config/ServiceConfig.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-public class ServiceConfig {
-    @Bean(initMethod = "init", destroyMethod = "destroy")
-    public ActivationCustomerService activationCustomerService(Notifier notifier) {
-        return new ActivationCustomerService(notifier);
-    }
-}
+//@Configuration
+//public class ServiceConfig {
+//    @Bean(initMethod = "init", destroyMethod = "destroy")
+//    public ActivationCustomerService activationCustomerService(Notifier notifier) {
+//        return new ActivationCustomerService(notifier);
+//    }
+//}

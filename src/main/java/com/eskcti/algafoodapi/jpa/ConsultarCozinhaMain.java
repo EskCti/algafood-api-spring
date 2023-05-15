8f5e85523054a9ad64ac8b5fb4618b3512220450
eskokado <eskokado@gmail.com>
2023-05-15 08:06:25 -0300

    feat: create poc class consultar cozinha main

---
+++ src/main/java/com/eskcti/algafoodapi/jpa/ConsultarCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.eskcti.algafoodapi.jpa;
+
+import com.eskcti.algafoodapi.AlgafoodApiApplication;
+import com.eskcti.algafoodapi.domain.models.Kitchen;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultarCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        List<Kitchen> kitchens = cadastroCozinha.listar();
+
+        for (Kitchen kitchen : kitchens) {
+            System.out.println(kitchen.getName());
+        }
+    }
+}

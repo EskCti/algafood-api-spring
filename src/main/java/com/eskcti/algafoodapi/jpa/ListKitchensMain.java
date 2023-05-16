4836e0535b95a8701a1a4388caa00156f5bf0a44
eskokado <eskokado@gmail.com>
2023-05-16 07:34:32 -0300

    refactor: impl repository KitchenRepository into ListKitchenMain

--- src/main/java/com/eskcti/algafoodapi/jpa/ListKitchensMain.java
+++ src/main/java/com/eskcti/algafoodapi/jpa/ListKitchensMain.java
@@ -2,21 +2,22 @@ package com.eskcti.algafoodapi.jpa;
 
 import com.eskcti.algafoodapi.AlgafoodApiApplication;
 import com.eskcti.algafoodapi.domain.models.Kitchen;
+import com.eskcti.algafoodapi.domain.repositories.KitchenRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
 
-public class ConsultarCozinhaMain {
+public class ListKitchensMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
                 .run(args);
 
-        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        KitchenRepository kitchenRepository = applicationContext.getBean(KitchenRepository.class);
 
-        List<Kitchen> kitchens = cadastroCozinha.listar();
+        List<Kitchen> kitchens = kitchenRepository.list();
 
         for (Kitchen kitchen : kitchens) {
             System.out.println(kitchen.getName());

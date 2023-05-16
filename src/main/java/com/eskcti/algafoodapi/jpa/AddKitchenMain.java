e3dba9ee589d8851c1d1d782f307d594d98d55e2
eskokado <eskokado@gmail.com>
2023-05-16 07:34:03 -0300

    refactor: impl repository KitchenRepository into AddKitchenMain

--- src/main/java/com/eskcti/algafoodapi/jpa/AddKitchenMain.java
+++ src/main/java/com/eskcti/algafoodapi/jpa/AddKitchenMain.java
@@ -2,17 +2,18 @@ package com.eskcti.algafoodapi.jpa;
 
 import com.eskcti.algafoodapi.AlgafoodApiApplication;
 import com.eskcti.algafoodapi.domain.models.Kitchen;
+import com.eskcti.algafoodapi.domain.repositories.KitchenRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AddKitchenMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
                 .run(args);
 
-        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        KitchenRepository kitchenRepository = applicationContext.getBean(KitchenRepository.class);
 
         Kitchen kitchen1 = new Kitchen();
         kitchen1.setName("Brasileira");
@@ -20,8 +21,8 @@ public class InclusaoCozinhaMain {
         Kitchen kitchen2 = new Kitchen();
         kitchen2.setName("Italiano");
 
-        kitchen1 = cadastroCozinha.save(kitchen1);
-        kitchen2 = cadastroCozinha.save(kitchen2);
+        kitchen1 = kitchenRepository.save(kitchen1);
+        kitchen2 = kitchenRepository.save(kitchen2);
 
         System.out.printf("%d - %s\n", kitchen1.getId(), kitchen1.getName());
         System.out.printf("%d - %s\n", kitchen2.getId(), kitchen2.getName());

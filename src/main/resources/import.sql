129b3febbd28f40dc4ff0f33aa9063ae593396fc
eskokado <eskokado@gmail.com>
2023-05-16 08:28:26 -0300

    chore: add insert into tab_restaurant

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO tab_kitchens (name) VALUES ("Japonês")
 INSERT INTO tab_kitchens (name) VALUES ("Indiana")
 INSERT INTO tab_kitchens (name) VALUES ("Chines")
+
+INSERT INTO tab_restaurants (name, shipping_fee) VALUES ("Restaurant 1", 5)
+INSERT INTO tab_restaurants (name, shipping_fee) VALUES ("Restaurant 2", 10)

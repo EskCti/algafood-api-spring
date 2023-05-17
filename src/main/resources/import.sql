bf9cfc88a7955789db7e342be6d992eb51f87f74
eskokado <eskokado@gmail.com>
2023-05-17 04:28:06 -0300

    fix: update tab_kitchens and tab_restaurants adding kitchen_id

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tab_kitchens (name) VALUES ("Japonês")
-INSERT INTO tab_kitchens (name) VALUES ("Indiana")
-INSERT INTO tab_kitchens (name) VALUES ("Chines")
+INSERT INTO tab_kitchens (id, name) VALUES (1, "Japonês")
+INSERT INTO tab_kitchens (id, name) VALUES (2, "Indiana")
+INSERT INTO tab_kitchens (id, name) VALUES (3, "Chines")
 
-INSERT INTO tab_restaurants (name, shipping_fee) VALUES ("Restaurant 1", 5)
-INSERT INTO tab_restaurants (name, shipping_fee) VALUES ("Restaurant 2", 10)
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 1", 5, 1)
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 2", 10, 2)

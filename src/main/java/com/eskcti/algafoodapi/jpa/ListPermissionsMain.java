1821b1aef22961a3ad6ac0fc2723337f79cc9ff3
eskokado <eskokado@gmail.com>
2023-05-17 07:21:38 -0300

    feat: create jpa ListPermissionsMain

---
+++ src/main/java/com/eskcti/algafoodapi/jpa/ListPermissionsMain.java
@@ -0,0 +1,26 @@
+package com.eskcti.algafoodapi.jpa;
+
+import com.eskcti.algafoodapi.AlgafoodApiApplication;
+import com.eskcti.algafoodapi.domain.models.Permission;
+import com.eskcti.algafoodapi.domain.repositories.PermissionRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ListPermissionsMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        PermissionRepository permissionRepository = applicationContext.getBean(PermissionRepository.class);
+
+        List<Permission> permissions = permissionRepository.list();
+
+        for (Permission permission : permissions) {
+            System.out.println(permission.getName());
+        }
+    }
+}

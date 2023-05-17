e8dbe26a41e63951a621df97839642f3aaa73017
eskokado <eskokado@gmail.com>
2023-05-17 06:58:05 -0300

    feat: create interface StateRepository

---
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/StateRepository.java
@@ -0,0 +1,12 @@
+package com.eskcti.algafoodapi.domain.repositories;
+
+import com.eskcti.algafoodapi.domain.models.State;
+
+import java.util.List;
+
+public interface StateRepository {
+    List<State> list();
+    State find(Long id);
+    State save(State state);
+    void remove(State state);
+}

53527e392fa4c03d14a8deb29027e70b8966cf80
eskokado <eskokado@gmail.com>
2023-05-19 06:47:14 -0300

    fix: add response entity

--- src/main/java/com/eskcti/algafoodapi/api/resources/KitchenController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/KitchenController.java
@@ -3,7 +3,9 @@ package com.eskcti.algafoodapi.api.resources;
 import com.eskcti.algafoodapi.domain.models.Kitchen;
 import com.eskcti.algafoodapi.domain.repositories.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +23,8 @@ public class KitchenController {
     }
 
     @GetMapping("/{id}")
-    public Kitchen find(@PathVariable Long id) {
-        return kitchenRepository.find(id);
+    public ResponseEntity<Kitchen> find(@PathVariable Long id) {
+        Kitchen kitchen = kitchenRepository.find(id);
+        return ResponseEntity.status(HttpStatus.OK).body(kitchen);
     }
 }

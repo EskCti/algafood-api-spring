ec51ba842a5616002c17cc3f42c68f40638b7f31
eskokado <eskokado@gmail.com>
2023-05-20 08:08:47 -0300

    feat: create restaurant controller

---
+++ src/main/java/com/eskcti/algafoodapi/api/resources/RestaurantController.java
@@ -0,0 +1,67 @@
+package com.eskcti.algafoodapi.api.resources;
+
+import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
+import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
+import com.eskcti.algafoodapi.domain.models.Restaurant;
+import com.eskcti.algafoodapi.domain.services.RestaurantService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/restaurants", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+public class RestaurantController {
+    @Autowired
+    RestaurantService restaurantService;
+
+    @GetMapping
+    public List<Restaurant> list() {
+        return restaurantService.list();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> find(@PathVariable Long id) {
+        try {
+            Restaurant restaurant = restaurantService.find(id);
+            return ResponseEntity.ok(restaurant);
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurant insert(@RequestBody Restaurant restaurant) {
+        return restaurantService.save(restaurant);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Restaurant> update(@PathVariable Long id, @RequestBody Restaurant restaurant) {
+        try {
+            Restaurant restaurantUpdated = restaurantService.find(id);
+            BeanUtils.copyProperties(restaurant, restaurantUpdated, "id");
+            restaurantService.save(restaurantUpdated);
+            return ResponseEntity.ok(restaurantUpdated);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity remove(@PathVariable Long id) {
+        try {
+            restaurantService.remove(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (EntityInUseException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+}

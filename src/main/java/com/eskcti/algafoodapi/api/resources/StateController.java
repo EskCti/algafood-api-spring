d8b3ef5e74eee351e147673f0ba9531a76de6807
eskokado <eskokado@gmail.com>
2023-05-20 11:16:14 -0300

    feat: create state controller with post, put, delete, gets

--- src/main/java/com/eskcti/algafoodapi/api/resources/StateController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/StateController.java
@@ -1,12 +1,15 @@
 package com.eskcti.algafoodapi.api.resources;
 
+import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
+import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
 import com.eskcti.algafoodapi.domain.models.State;
-import com.eskcti.algafoodapi.domain.repositories.StateRepository;
+import com.eskcti.algafoodapi.domain.services.StateService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,10 +17,50 @@ import java.util.List;
 @RequestMapping(value = "/states", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class StateController {
     @Autowired
-    private StateRepository stateRepository;
+    private StateService stateService;
 
     @GetMapping
     public List<State> list() {
-        return stateRepository.list();
+        return stateService.list();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Long id) {
+        try {
+            State state = stateService.find(id);
+            return ResponseEntity.ok(state);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insert(@RequestBody State state) {
+        state = stateService.save(state);
+        return ResponseEntity.status(HttpStatus.CREATED).body(state);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody State state) {
+        try {
+            State stateUpdate = stateService.find(id);
+            BeanUtils.copyProperties(state, stateUpdate, "id");
+            stateUpdate = stateService.save(stateUpdate);
+            return ResponseEntity.ok(stateUpdate);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            stateService.remove(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (EntityInUseException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
     }
 }

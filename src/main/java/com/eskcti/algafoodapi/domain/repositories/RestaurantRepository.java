74587b997420d978e934e953efcf5bd86ed43c55
eskokado <eskokado@gmail.com>
2023-05-22 08:08:33 -0300

    feat: add examples queries methods with prefix into restaurant repository

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository  extends JpaRepository<Restaurant, Long> {
     List<Restaurant> findByShippingFeeBetween(BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
 
     List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchenId);
+
+    Optional<Restaurant> findFirstQualquerCoisaByNameContaining(String name);
+
+    List<Restaurant> findTop2ByNameContaining(String name);
+
+    boolean existsByName(String name);
+
+    int countByKitchenId(Long kitchenId);
 }

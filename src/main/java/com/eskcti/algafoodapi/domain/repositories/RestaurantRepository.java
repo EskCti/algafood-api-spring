57b39e96e7f3a9ee8af87b4c1d605a6cfbea3013
eskokado <eskokado@gmail.com>
2023-05-22 06:32:14 -0300

    feat: add findByShippingFeeBetween and findByNameContainingAndKitchenId into restaurant repository

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
@@ -4,8 +4,12 @@ import com.eskcti.algafoodapi.domain.models.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface RestaurantRepository  extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByShippingFeeBetween(BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
+
+    List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchenId);
 }

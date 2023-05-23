f98f18ac460da76d0fee7adb53a3581a4aace245
eskokado <eskokado@gmail.com>
2023-05-23 06:34:21 -0300

    feat: create query dynamic by name containing and shipping fee between into restaurant repository impl

--- src/main/java/com/eskcti/algafoodapi/infrastruct/repositories/RestaurantRepositoryImpl.java
+++ src/main/java/com/eskcti/algafoodapi/infrastruct/repositories/RestaurantRepositoryImpl.java
@@ -4,9 +4,12 @@ import com.eskcti.algafoodapi.domain.models.Restaurant;
 import com.eskcti.algafoodapi.domain.repositories.RestaurantRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -16,13 +19,36 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
 
     public List<Restaurant> find(String name,
                                  BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal) {
-        var jpql = "from Restaurant where name like :name " +
-                "and shippingFee between :shippingFeeInitial and :shippingFeeFinal";
+//        var jpql = "from Restaurant where name like :name " +
+//                "and shippingFee between :shippingFeeInitial and :shippingFeeFinal";
 
-        return manager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%" + name + "%")
-                .setParameter("shippingFeeInitial", shippingFeeInitial)
-                .setParameter("shippingFeeFinal", shippingFeeFinal)
-                .getResultList();
+        var jpql = new StringBuilder();
+        var params = new HashMap<String, Object>();
+        jpql.append("from Restaurant where 0 = 0 ");
+
+        if(StringUtils.hasLength(name)) {
+            jpql.append("and name like :name ");
+            params.put("name", "%" + name + "%");
+        }
+
+        if (shippingFeeInitial != null) {
+            jpql.append("and shippingFee >= :shippingFeeInitial ");
+            params.put("shippingFeeInitial", shippingFeeInitial);
+        }
+
+        if (shippingFeeFinal != null) {
+            jpql.append("and shippingFee <= :shippingFeeFinal ");
+            params.put("shippingFeeFinal", shippingFeeFinal);
+        }
+
+//        return manager.createQuery(jpql.toString(), Restaurant.class)
+//                .setParameter("name", "%" + name + "%")
+//                .setParameter("shippingFeeInitial", shippingFeeInitial)
+//                .setParameter("shippingFeeFinal", shippingFeeFinal)
+//                .getResultList();
+
+        TypedQuery<Restaurant> query = manager.createQuery(jpql.toString(), Restaurant.class);
+        params.forEach((key, value) -> query.setParameter(key, value));
+        return query.getResultList();
     }
 }

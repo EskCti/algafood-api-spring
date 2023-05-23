f021dda1acc2556cb16b162769e7d548c94f0ad3
eskokado <eskokado@gmail.com>
2023-05-23 05:59:20 -0300

    feat: create query restaurant by name and shipping fee between into interface RestaurantRepositoryQueries

---
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepositoryQueries.java
@@ -0,0 +1,11 @@
+package com.eskcti.algafoodapi.domain.repositories;
+
+import com.eskcti.algafoodapi.domain.models.Restaurant;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestaurantRepositoryQueries {
+    List<Restaurant> find(String name,
+                          BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
+}

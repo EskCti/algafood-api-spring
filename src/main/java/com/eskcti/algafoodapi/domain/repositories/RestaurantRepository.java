777884082023a7b4220a63b6e90930558890aa17
eskokado <eskokado@gmail.com>
2023-05-24 06:54:01 -0300

    feat: add extends JpaSpecificationExecutor

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.eskcti.algafoodapi.domain.repositories;
 
 import com.eskcti.algafoodapi.domain.models.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RestaurantRepository  extends JpaRepository<Restaurant, Long>, RestaurantRepositoryQueries {
+public interface RestaurantRepository  extends
+        JpaRepository<Restaurant, Long>,
+        RestaurantRepositoryQueries,
+        JpaSpecificationExecutor<Restaurant>
+{
     List<Restaurant> findByShippingFeeBetween(BigDecimal shippingFeeInitial, BigDecimal shippingFeeFinal);
 
 //    @Query("from Restaurant where name like %:name% and kitchen.id = :id")

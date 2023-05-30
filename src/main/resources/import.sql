00170afdc9b89e73bbbd8f029ac93837c20f8e79
eskokado <eskokado@gmail.com>
2023-05-30 15:06:38 -0300

    feat: impact of class embedding on REST API

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,11 +2,6 @@ INSERT INTO tab_kitchens (id, name) VALUES (1, "Japonês");
 INSERT INTO tab_kitchens (id, name) VALUES (2, "Indiana");
 INSERT INTO tab_kitchens (id, name) VALUES (3, "Chines");
 
-INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 1", 5, 1);
-INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 2", 10, 2);
-INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 3", 0, 3);
-INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 4", 0, 3);
-
 INSERT INTO tab_states (id, name) VALUE (1, "Minas Gerais");
 INSERT INTO tab_states (id, name) VALUE (2, "São Paulo");
 INSERT INTO tab_states (id, name) VALUE (3, "Ceara");
@@ -16,6 +11,11 @@ INSERT INTO tab_cities (id, name, state_id) VALUES (2, "Belo Horizonte", 1);
 INSERT INTO tab_cities (id, name, state_id) VALUES (3, "São Paulo", 2);
 INSERT INTO tab_cities (id, name, state_id) VALUES (4, "Campinas", 2);
 INSERT INTO tab_cities (id, name, state_id) VALUES (5, "Fortaleza", 3);
+
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id, zip_code, public_place, number, complement, neighborhood, city_id) VALUES ("Restaurant 1", 5, 1, "12345678", "logradouro tal", "123", "apto 25", "santana", 1);
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 2", 10, 2);
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 3", 0, 3);
+INSERT INTO tab_restaurants (name, shipping_fee, kitchen_id) VALUES ("Restaurant 4", 0, 3);
 
 INSERT INTO tab_payments_type (id, description) VALUES (1, "Cartão de crédito");
 INSERT INTO tab_payments_type (id, description) VALUES (2, "Cartão de débito");

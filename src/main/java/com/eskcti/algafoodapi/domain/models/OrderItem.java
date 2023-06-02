4adc1ead140d5d9cbe6bc7d96a01ab40ea0af921
eskokado <eskokado@gmail.com>
2023-06-02 05:41:28 -0300

    feat: create entity order item

---
+++ src/main/java/com/eskcti/algafoodapi/domain/models/OrderItem.java
@@ -0,0 +1,37 @@
+package com.eskcti.algafoodapi.domain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tab_orders_items")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price_unit", nullable = false)
+    private BigDecimal priceUnit;
+
+    @Column(name = "price_total", nullable = false)
+    private BigDecimal priceTotal;
+
+    private String observation;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Product product;
+}

0e33dcfd9b3ef4fbf0e60c79954328577076528a
eskokado <eskokado@gmail.com>
2023-06-04 07:02:03 -0300

    refactor: impl exception KitchenNotFoundException

--- src/main/java/com/eskcti/algafoodapi/domain/services/KitchenService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/KitchenService.java
@@ -1,19 +1,17 @@
 package com.eskcti.algafoodapi.domain.services;
 
 import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
-import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
+import com.eskcti.algafoodapi.domain.exceptions.KitchenNotFoundException;
 import com.eskcti.algafoodapi.domain.models.Kitchen;
 import com.eskcti.algafoodapi.domain.repositories.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class KitchenService {
-    public static final String KITCHEN_NOT_FOUND = "Kitchen with id %d not found";
     public static final String KITCHEN_NOT_REMOVED_IN_USE = "Kitchen with id %d not removed in use ";
     @Autowired
     private KitchenRepository kitchenRepository;
@@ -32,7 +30,7 @@ public class KitchenService {
 
     public Kitchen find(Long id) {
         return kitchenRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(String.format(KITCHEN_NOT_FOUND, id)));
+                .orElseThrow(() -> new KitchenNotFoundException(id));
     }
 
     public List<Kitchen> list() {

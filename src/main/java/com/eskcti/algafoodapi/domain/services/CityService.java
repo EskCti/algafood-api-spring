f3c55193a5a091ffa4cee676bc58e6b541dc90ea
eskokado <eskokado@gmail.com>
2023-06-04 06:38:44 -0300

    refactor: impl exception StateNotFoundException int CityService

--- src/main/java/com/eskcti/algafoodapi/domain/services/CityService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/CityService.java
@@ -2,6 +2,7 @@ package com.eskcti.algafoodapi.domain.services;
 
 import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
 import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
+import com.eskcti.algafoodapi.domain.exceptions.StateNotFoundException;
 import com.eskcti.algafoodapi.domain.models.City;
 import com.eskcti.algafoodapi.domain.models.State;
 import com.eskcti.algafoodapi.domain.repositories.CityRepository;
@@ -17,7 +18,6 @@ import java.util.List;
 public class CityService {
     public static final String NOT_FOUND_STATE_INTO_CITY_REQUEST = "Not found state into city request";
     public static final String NOT_FOUND_STATE_WITHOUT_ID = "Not found state without id";
-    public static final String NOT_FOUND_STATE_WITH_ID = "Not found State with id %d";
     public static final String NOT_REMOVED_IN_USE = "City with id %d not removed in use";
     public static final String NOT_FOUND_CITY_WITH_ID = "Not found city with id %d";
     @Autowired
@@ -35,18 +35,18 @@ public class CityService {
         }
         Long stateId = city.getState().getId();
         if (stateId == null) {
-            throw new EntityNotFoundException(NOT_FOUND_STATE_WITHOUT_ID);
+            throw new StateNotFoundException(NOT_FOUND_STATE_WITHOUT_ID);
         }
         try {
             State state = stateRepository.findById(stateId)
                     .orElseThrow(() ->
-                            new EntityNotFoundException(String.format(NOT_FOUND_STATE_WITH_ID, stateId)
-                            )
+                            new StateNotFoundException(stateId)
                     );
+
             city.setState(state);
             return cityRepository.save(city);
         } catch (EmptyResultDataAccessException e) {
-            throw new EntityNotFoundException(String.format(NOT_FOUND_STATE_WITH_ID, stateId));
+            throw new StateNotFoundException(stateId);
         }
     }
 

e908f7a044c566ea35685438eceaa12ecd5b9b9b
eskokado <eskokado@gmail.com>
2023-06-05 05:44:56 -0300

    feat: create handle exception with @ExceptionHandler

--- src/main/java/com/eskcti/algafoodapi/api/resources/CityController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/CityController.java
@@ -1,6 +1,8 @@
 package com.eskcti.algafoodapi.api.resources;
 
+import com.eskcti.algafoodapi.api.exceptionhandler.Problem;
 import com.eskcti.algafoodapi.domain.exceptions.BusinessException;
+import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
 import com.eskcti.algafoodapi.domain.exceptions.StateNotFoundException;
 import com.eskcti.algafoodapi.domain.models.City;
 import com.eskcti.algafoodapi.domain.services.CityService;
@@ -8,8 +10,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -53,5 +57,25 @@ public class CityController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         cityService.remove(id);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        Problem problem = Problem.builder()
+                .dateTime(LocalDateTime.now())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problem);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<?> handleBusinessException(BusinessException e) {
+        Problem problem = Problem.builder()
+                .dateTime(LocalDateTime.now())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(problem);
     }
 }

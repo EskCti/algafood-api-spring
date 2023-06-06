25ade8de4bf4991b495a9c140a5141d2558ff628
eskokado <eskokado@gmail.com>
2023-06-06 06:56:52 -0300

    feat: custom ApiExceptionHandler extends ResponseEntityExceptionHandler

--- src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,54 +3,50 @@ package com.eskcti.algafoodapi.api.exceptionhandler;
 import com.eskcti.algafoodapi.domain.exceptions.BusinessException;
 import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
 import com.eskcti.algafoodapi.domain.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
-        Problem problem = Problem.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problem);
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e, WebRequest request) {
+
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<?> handleBusinessException(BusinessException e) {
-        Problem problem = Problem.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problem);
+    public ResponseEntity<?> handleBusinessException(BusinessException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
 
     @ExceptionHandler(EntityInUseException.class)
-    public ResponseEntity<?> handleEntityInUseException(EntityInUseException e) {
-        Problem problem = Problem.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(problem);
+    public ResponseEntity<?> handleEntityInUseException(EntityInUseException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-        Problem problem = Problem.builder()
-                .dateTime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(problem);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        if (body == null) {
+            body = Problem.builder()
+                    .dateTime(LocalDateTime.now())
+                    .message(ex.getMessage())
+                    .build();
+        } else {
+            body = Problem.builder()
+                    .dateTime(LocalDateTime.now())
+                    .message((String) body)
+                    .build();
+        }
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 }

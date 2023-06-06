428e71edc1cb52b25c4493ab9fdad49af9ae0d60
eskokado <eskokado@gmail.com>
2023-06-06 07:48:11 -0300

    feat: impl method custom createProblemBuilder

--- src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -7,20 +7,22 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ProblemType problemType = ProblemType.ENTITY_NOT_FOUND;
+        String detail = e.getMessage();
 
-        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(BusinessException.class)
@@ -38,15 +40,23 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
         if (body == null) {
             body = Problem.builder()
-                    .dateTime(LocalDateTime.now())
-                    .message(ex.getMessage())
+                    .title(ex.getMessage())
+                    .status(statusCode.value())
                     .build();
-        } else {
+        } else if (body instanceof String) {
             body = Problem.builder()
-                    .dateTime(LocalDateTime.now())
-                    .message((String) body)
+                    .title(ex.getMessage())
+                    .status(statusCode.value())
                     .build();
         }
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+
+    private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+        return Problem.builder()
+                .status(status.value())
+                .type(problemType.getUri())
+                .title(problemType.getTitle())
+                .detail(detail);
     }
 }

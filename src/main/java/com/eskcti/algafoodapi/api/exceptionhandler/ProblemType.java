5dbad40fe2e467dca283a0aea6b7cf8ad1965204
eskokado <eskokado@gmail.com>
2023-06-07 04:35:03 -0300

    feat: add new item ProblemType

--- src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/eskcti/algafoodapi/api/exceptionhandler/ProblemType.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
+    INCOMPREHENSIBLE_MESSAGE("/incomprehensible-message", "Incomprehensible message"),
     ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"),
     ENTITY_IN_USE("/entity-in-use", "Entity in use"),
     ERROR_BUSINESS("/error_business", "Error business");

c87222b761435770450828e254aa1ecdcf7a3874
eskokado <eskokado@gmail.com>
2023-06-09 06:12:27 -0300

    feat: add validation constraints in model kitchen

--- src/main/java/com/eskcti/algafoodapi/domain/models/Kitchen.java
+++ src/main/java/com/eskcti/algafoodapi/domain/models/Kitchen.java
@@ -1,6 +1,9 @@
 package com.eskcti.algafoodapi.domain.models;
 
+import com.eskcti.algafoodapi.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -11,10 +14,13 @@ import java.util.Objects;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Kitchen {
+    @NotNull(groups = Groups.RestaurantRegistration.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     @Column(nullable = false, length = 100)
     private String name;
 }

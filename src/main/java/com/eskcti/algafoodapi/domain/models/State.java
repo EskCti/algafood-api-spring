8aaa26fd4ea8dade1f526f0f6499a68bf4126854
eskokado <eskokado@gmail.com>
2023-06-10 06:04:18 -0300

    feat: add annotation validation into model state

--- src/main/java/com/eskcti/algafoodapi/domain/models/State.java
+++ src/main/java/com/eskcti/algafoodapi/domain/models/State.java
@@ -1,6 +1,9 @@
 package com.eskcti.algafoodapi.domain.models;
 
+import com.eskcti.algafoodapi.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -12,7 +15,10 @@ public class State {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(groups = Groups.StateId.class)
     private Long id;
+
+    @NotBlank
     @Column(nullable = false, length = 60)
     private String name;
 }

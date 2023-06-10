8490e793603583453ceea72ad6ea4153869dd79a
eskokado <eskokado@gmail.com>
2023-06-10 06:04:27 -0300

    feat: add annotation validation into model city

--- src/main/java/com/eskcti/algafoodapi/domain/models/City.java
+++ src/main/java/com/eskcti/algafoodapi/domain/models/City.java
@@ -1,6 +1,12 @@
 package com.eskcti.algafoodapi.domain.models;
 
+import com.eskcti.algafoodapi.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,9 +19,15 @@ public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     @Column(nullable = false, length = 100)
     private String name;
+
+    @Valid
     @ManyToOne
     @JoinColumn(name = "state_id", nullable = false)
+    @NotNull
+    @ConvertGroup(from = Default.class, to = Groups.StateId.class)
     private State state;
 }

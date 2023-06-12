2850018944aab63fbbd55e3ba3731309179c3601
eskokado <eskokado@gmail.com>
2023-06-12 05:21:34 -0300

    test: test Kitchen Registration Successfully

--- src/test/java/com/eskcti/algafoodapi/AlgafoodApiApplicationTests.java
+++ src/test/java/com/eskcti/algafoodapi/AlgafoodApiApplicationTests.java
@@ -1,16 +1,28 @@
 package com.eskcti.algafoodapi;
 
+import com.eskcti.algafoodapi.domain.models.Kitchen;
+import com.eskcti.algafoodapi.domain.services.KitchenService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+class RegisterKitchenIntegrationTests {
+
+	@Autowired
+	KitchenService kitchenService;
 
 	@Test
-	void contextLoads() {
-		Assertions.assertTrue(1 == 1);
-	}
+	public void testKitchenRegistrationSuccessfully() {
+		Kitchen newKitchen = new Kitchen();
+		newKitchen.setName("Chinesa");
 
+		newKitchen = kitchenService.save(newKitchen);
+
+		assertThat(newKitchen, notNullValue());
+		assertThat(newKitchen.getId(), notNullValue());
+	}
 }

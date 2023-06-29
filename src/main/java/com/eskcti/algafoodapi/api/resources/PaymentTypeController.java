4c9bf885ded5b0888ae968cc928f46e6d6e449be
eskokado <eskokado@gmail.com>
2023-06-29 06:07:52 -0300

    feat: add method GET find by id into Payment Type Controller

--- src/main/java/com/eskcti/algafoodapi/api/resources/PaymentTypeController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/PaymentTypeController.java
@@ -1,10 +1,13 @@
 package com.eskcti.algafoodapi.api.resources;
 
+import com.eskcti.algafoodapi.api.assembliers.PaymentTypeModelAssemblier;
+import com.eskcti.algafoodapi.api.model.PaymentTypeModel;
 import com.eskcti.algafoodapi.domain.models.PaymentType;
 import com.eskcti.algafoodapi.domain.services.PaymentTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,18 @@ public class PaymentTypeController {
     @Autowired
     private PaymentTypeService paymentTypeService;
 
+    @Autowired
+    private PaymentTypeModelAssemblier modelAssemblier;
+
     @GetMapping
     public List<PaymentType> list() {
         return paymentTypeService.list();
     }
+
+    @GetMapping("/{id}")
+    public PaymentTypeModel find(@PathVariable Long id) {
+        return modelAssemblier.toModel(paymentTypeService.find(id));
+    }
+
+
 }

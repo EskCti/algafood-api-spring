8089fb5396b08a31c68a9a4743d244c8ed56b29b
eskokado <eskokado@gmail.com>
2023-06-29 05:28:51 -0300

    feat: add method find into Payment Type Service

--- src/main/java/com/eskcti/algafoodapi/domain/services/PaymentTypeService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/PaymentTypeService.java
@@ -1,5 +1,6 @@
 package com.eskcti.algafoodapi.domain.services;
 
+import com.eskcti.algafoodapi.domain.exceptions.PaymentTypeNotFoundException;
 import com.eskcti.algafoodapi.domain.models.PaymentType;
 import com.eskcti.algafoodapi.domain.repositories.PaymentTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 public class PaymentTypeService {
+    public static final String PAYMENT_TYPE_NOT_FOUND = "Payment type not found with id %d";
     @Autowired
     private PaymentTypeRepository paymentTypeRepository;
 
@@ -22,5 +24,10 @@ public class PaymentTypeService {
         return paymentTypeRepository.findAll();
     }
 
-
+    public PaymentType find(Long id) {
+        PaymentType paymentType = paymentTypeRepository
+                .findById(id)
+                .orElseThrow(() -> new PaymentTypeNotFoundException(String.format(PAYMENT_TYPE_NOT_FOUND, id)));
+        return paymentType;
+    }
 }

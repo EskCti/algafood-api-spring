8de8f18abb1ca962d891aafc05f2db2e58bfedd0
eskokado <eskokado@gmail.com>
2023-06-29 06:20:23 -0300

    fix: add annotation @NotBlank in description of PaymentTypeInput

--- src/main/java/com/eskcti/algafoodapi/api/model/input/PaymentTypeInput.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/input/PaymentTypeInput.java
@@ -1,5 +1,6 @@
 package com.eskcti.algafoodapi.api.model.input;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 public class PaymentTypeInput {
 
     @NotNull
+    @NotBlank
     private String description;
 }

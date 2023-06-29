f38929bf17f92f70c7ad407fe165e5cb0a5226d1
eskokado <eskokado@gmail.com>
2023-06-29 05:33:59 -0300

    feat: add method remove into Payment Type Service

--- src/main/java/com/eskcti/algafoodapi/domain/services/PaymentTypeService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/PaymentTypeService.java
@@ -1,9 +1,11 @@
 package com.eskcti.algafoodapi.domain.services;
 
+import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
 import com.eskcti.algafoodapi.domain.exceptions.PaymentTypeNotFoundException;
 import com.eskcti.algafoodapi.domain.models.PaymentType;
 import com.eskcti.algafoodapi.domain.repositories.PaymentTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Service
 public class PaymentTypeService {
     public static final String PAYMENT_TYPE_NOT_FOUND = "Payment type not found with id %d";
+    public static final String PAYMENT_TYPE_NOT_REMOVED_IN_USE = "Payment type with id %d not removed in use ";
     @Autowired
     private PaymentTypeRepository paymentTypeRepository;
 
@@ -29,5 +32,15 @@ public class PaymentTypeService {
                 .findById(id)
                 .orElseThrow(() -> new PaymentTypeNotFoundException(String.format(PAYMENT_TYPE_NOT_FOUND, id)));
         return paymentType;
+    }
+
+    @Transactional
+    public void remove(Long id) {
+        try {
+            paymentTypeRepository.deleteById(id);
+            paymentTypeRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            throw new EntityInUseException(String.format(PAYMENT_TYPE_NOT_REMOVED_IN_USE, id));
+        }
     }
 }

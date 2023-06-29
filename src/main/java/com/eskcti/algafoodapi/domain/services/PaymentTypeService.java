fe4d67440feed94a57c62564be0c5d42794a1434
eskokado <eskokado@gmail.com>
2023-06-29 05:12:38 -0300

    feat: create service Payment Type and add method save

---
+++ src/main/java/com/eskcti/algafoodapi/domain/services/PaymentTypeService.java
@@ -0,0 +1,20 @@
+package com.eskcti.algafoodapi.domain.services;
+
+import com.eskcti.algafoodapi.domain.models.PaymentType;
+import com.eskcti.algafoodapi.domain.repositories.PaymentTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PaymentTypeService {
+    @Autowired
+    private PaymentTypeRepository paymentTypeRepository;
+
+    @Transactional
+    public PaymentType save(PaymentType paymentType) {
+        return paymentTypeRepository.save(paymentType);
+    }
+
+
+}

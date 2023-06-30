1c0e1d6d32812e95315ca7042272a39708aeb4e9
eskokado <eskokado@gmail.com>
2023-06-30 07:19:37 -0300

    feat: create group repository

---
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.eskcti.algafoodapi.domain.repositories;
+
+import com.eskcti.algafoodapi.domain.models.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
+}

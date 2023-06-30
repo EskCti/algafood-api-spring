b2bf20ed13f8ec1693ad4d8bd04cf2808782ed70
eskokado <eskokado@gmail.com>
2023-06-30 07:42:28 -0300

    feat: add GET find group into GroupController

--- src/main/java/com/eskcti/algafoodapi/api/resources/GroupController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/GroupController.java
@@ -7,6 +7,7 @@ import com.eskcti.algafoodapi.domain.services.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class GroupController {
     @GetMapping
     public List<GroupModel> list() {
         return modelAssemblier.toCollectionModel(groupService.list());
+    }
+
+    @GetMapping("/{id}")
+    public GroupModel find(@PathVariable Long id) {
+        return modelAssemblier.toModel(groupService.find(id));
     }
 }

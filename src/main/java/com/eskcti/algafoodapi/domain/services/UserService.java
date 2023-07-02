23ba67f6dc7a2e35aa9e09403fbabb9b9168a0a7
eskokado <eskokado@gmail.com>
2023-07-02 12:22:43 -0300

    feat: add method update password into UserService

--- src/main/java/com/eskcti/algafoodapi/domain/services/UserService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/UserService.java
@@ -1,5 +1,6 @@
 package com.eskcti.algafoodapi.domain.services;
 
+import com.eskcti.algafoodapi.domain.exceptions.BusinessException;
 import com.eskcti.algafoodapi.domain.exceptions.UserNotFoundException;
 import com.eskcti.algafoodapi.domain.models.User;
 import com.eskcti.algafoodapi.domain.repositories.UserRepository;
@@ -33,5 +34,17 @@ public class UserService {
     public void delete(Long id) {
         User user = find(id);
         userRepository.delete(user);
+    }
+
+    @Transactional
+    public User updatePassword(Long id, String currentPassword, String newPassword) {
+        User user = find(id);
+        System.out.println(user.getPassword());
+        System.out.println(currentPassword);
+        if (user.passwordNotMatchWith(currentPassword)) {
+            throw new BusinessException("Current password entered does not match the user's password");
+        }
+        user.setPassword(newPassword);
+        return userRepository.save(user);
     }
 }

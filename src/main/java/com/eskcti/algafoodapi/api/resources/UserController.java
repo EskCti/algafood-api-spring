5bcba04e07dbecabf34740686455beadecb79c94
eskokado <eskokado@gmail.com>
2023-07-02 07:28:02 -0300

    feat: add POST insert User into UserController

--- src/main/java/com/eskcti/algafoodapi/api/resources/UserController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/UserController.java
@@ -1,13 +1,15 @@
 package com.eskcti.algafoodapi.api.resources;
 
+import com.eskcti.algafoodapi.api.assembliers.UserInputDisassembler;
 import com.eskcti.algafoodapi.api.assembliers.UserModelAssemblier;
 import com.eskcti.algafoodapi.api.model.UserModel;
+import com.eskcti.algafoodapi.api.model.input.UserInput;
+import com.eskcti.algafoodapi.domain.models.User;
 import com.eskcti.algafoodapi.domain.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserModelAssemblier modelAssemblier;
+    @Autowired
+    private UserInputDisassembler inputDisassembler;
     @Autowired
     private UserService userService;
 
@@ -27,5 +31,12 @@ public class UserController {
     @GetMapping("/{id}")
     public UserModel find(@PathVariable Long id) {
         return modelAssemblier.toModel(userService.find(id));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserModel insert(@RequestBody @Valid UserInput userInput) {
+        User user = inputDisassembler.toDomainObject(userInput);
+        return modelAssemblier.toModel(userService.save(user));
     }
 }

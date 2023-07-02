cf31bb280c4fc4cab73506515f3327f643a37f14
eskokado <eskokado@gmail.com>
2023-07-02 07:22:40 -0300

    feat: add method save with annotation @Transacional into UserService

--- src/main/java/com/eskcti/algafoodapi/domain/services/UserService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/UserService.java
@@ -5,6 +5,7 @@ import com.eskcti.algafoodapi.domain.models.User;
 import com.eskcti.algafoodapi.domain.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +22,10 @@ public class UserService {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
         return user;
+    }
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
     }
 }

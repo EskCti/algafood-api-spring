a277bbbcc81820754165c3839c519290b069622f
eskokado <eskokado@gmail.com>
2023-07-04 04:24:47 -0300

    feat: add endpoint DELETE disassociate payment type into controller Restaurant Payment Type

--- src/main/java/com/eskcti/algafoodapi/api/resources/RestaurantPaymentTypeController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/RestaurantPaymentTypeController.java
@@ -5,11 +5,9 @@ import com.eskcti.algafoodapi.api.model.PaymentTypeModel;
 import com.eskcti.algafoodapi.domain.models.Restaurant;
 import com.eskcti.algafoodapi.domain.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,11 @@ public class RestaurantPaymentTypeController {
         Restaurant restaurant = restaurantService.find(restaurantId);
 
         return modelAssemblier.toCollectionModel(restaurant.getPaymentTypes());
+    }
+
+    @DeleteMapping("/{paymentTypeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disassociate(@PathVariable Long restaurantId, @PathVariable Long paymentTypeId) {
+        restaurantService.disassociatePaymentType(restaurantId, paymentTypeId);
     }
 }

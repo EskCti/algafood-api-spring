d40474a9ce53444fb86902f5f174568b59ce7c9b
eskokado <eskokado@gmail.com>
2023-07-04 06:30:27 -0300

    feat: add method delete product into ProductService

--- src/main/java/com/eskcti/algafoodapi/domain/services/ProductService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/ProductService.java
@@ -1,14 +1,18 @@
 package com.eskcti.algafoodapi.domain.services;
 
+import com.eskcti.algafoodapi.domain.exceptions.EntityInUseException;
 import com.eskcti.algafoodapi.domain.exceptions.ProductNotFoundException;
 import com.eskcti.algafoodapi.domain.exceptions.RestaurantAndProductNotFoundException;
 import com.eskcti.algafoodapi.domain.models.Product;
 import com.eskcti.algafoodapi.domain.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
+    public static final String PRODUCT_REMOVED_IN_USE = "Product with id %d not removed in use ";
     @Autowired
     private ProductRepository productRepository;
 
@@ -20,5 +24,16 @@ public class ProductService {
     public Product findByRestaurantIdAndId(Long restaurantId, Long productId) {
         return productRepository.findByRestaurantIdAndId(restaurantId, productId)
                 .orElseThrow(() -> new RestaurantAndProductNotFoundException(restaurantId, productId));
+    }
+
+    @Transactional
+    public void remove(Long id) {
+        try {
+            find(id);
+            productRepository.deleteById(id);
+            productRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            throw new EntityInUseException(String.format(PRODUCT_REMOVED_IN_USE, id));
+        }
     }
 }

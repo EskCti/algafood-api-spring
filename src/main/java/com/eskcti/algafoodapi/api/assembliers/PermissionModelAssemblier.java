f1e736e28099ed0e600f994441bfe8e0b4885dcd
eskokado <eskokado@gmail.com>
2023-07-05 05:18:07 -0300

    feat: create mapper PermissionModelAssemblier

---
+++ src/main/java/com/eskcti/algafoodapi/api/assembliers/PermissionModelAssemblier.java
@@ -0,0 +1,27 @@
+package com.eskcti.algafoodapi.api.assembliers;
+
+import com.eskcti.algafoodapi.api.model.GroupModel;
+import com.eskcti.algafoodapi.api.model.PermissionModel;
+import com.eskcti.algafoodapi.domain.models.Group;
+import com.eskcti.algafoodapi.domain.models.Permission;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PermissionModelAssemblier {
+    @Autowired
+    private ModelMapper modelMapper;
+    public PermissionModel toModel(Permission permission) {
+        return modelMapper.map(permission, PermissionModel.class);
+    }
+
+    public List<PermissionModel> toCollectionModel(List<Permission> permissions) {
+        return permissions.stream()
+                .map(permission -> toModel(permission))
+                .collect(Collectors.toList());
+    }
+}

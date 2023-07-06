a1a3d3af8f6b6bb73df42e5029e803aef56a13db
eskokado <eskokado@gmail.com>
2023-07-06 06:02:12 -0300

    feat: add endpoint PUT associate User with Group in UserController

--- src/main/java/com/eskcti/algafoodapi/api/resources/UserGroupController.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/UserGroupController.java
@@ -1,16 +1,13 @@
 package com.eskcti.algafoodapi.api.resources;
 
 import com.eskcti.algafoodapi.api.assembliers.GroupModelAssemblier;
-import com.eskcti.algafoodapi.api.assembliers.UserModelAssemblier;
 import com.eskcti.algafoodapi.api.model.GroupModel;
-import com.eskcti.algafoodapi.api.model.UserModel;
 import com.eskcti.algafoodapi.domain.models.User;
+import com.eskcti.algafoodapi.domain.services.GroupService;
 import com.eskcti.algafoodapi.domain.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +18,20 @@ public class UserGroupController {
     private UserService userService;
 
     @Autowired
+    private GroupService groupService;
+
+    @Autowired
     private GroupModelAssemblier modelAssemblier;
 
     @GetMapping
     public List<GroupModel> list(@PathVariable Long userId) {
         User user = userService.find(userId);
         return modelAssemblier.toCollectionModel(user.getGroups());
+    }
+
+    @PutMapping("/{groupId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void associate(@PathVariable Long userId, @PathVariable Long groupId) {
+        userService.associateGroup(userId, groupId);
     }
 }

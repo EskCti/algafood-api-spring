11f2c498167f5de48131cb802febfd3760ec0445
eskokado <eskokado@gmail.com>
2023-07-07 09:08:15 -0300

    feat: add method find Order in OrderService

---
+++ src/main/java/com/eskcti/algafoodapi/domain/services/OrderService.java
@@ -0,0 +1,20 @@
+package com.eskcti.algafoodapi.domain.services;
+
+import com.eskcti.algafoodapi.domain.exceptions.OrderNotFoundException;
+import com.eskcti.algafoodapi.domain.models.Order;
+import com.eskcti.algafoodapi.domain.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order find(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+    }
+}

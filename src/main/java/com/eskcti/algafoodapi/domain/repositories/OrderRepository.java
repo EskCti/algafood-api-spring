2743eca40caacf1396af904e41241f52ea21f37e
eskokado <eskokado@gmail.com>
2023-07-12 05:29:47 -0300

    feat: add findAll with query custom optimized

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/OrderRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/OrderRepository.java
@@ -1,6 +1,11 @@
 package com.eskcti.algafoodapi.domain.repositories;
 
 import com.eskcti.algafoodapi.domain.models.Order;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends CustomJpaRepository<Order, Long> {
+    @Query("from Order o join fetch o.customer c join fetch o.restaurant r join fetch r.kitchen")
+    List<Order> findAlll();
 }

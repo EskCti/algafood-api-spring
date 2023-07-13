03e05a271b65bed6490a186cd37890c4e772ac23
eskokado <eskokado@gmail.com>
2023-07-13 08:50:26 -0300

    refactor: add method cannot change to into OrderStatus

--- src/main/java/com/eskcti/algafoodapi/domain/models/OrderStatus.java
+++ src/main/java/com/eskcti/algafoodapi/domain/models/OrderStatus.java
@@ -1,18 +1,27 @@
 package com.eskcti.algafoodapi.domain.models;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum OrderStatus {
     CREATED("Created"),
-    CONFIRMED("Confirmed"),
-    DELIVERED("Delivered"),
-    CANCELED("Canceled");
+    CONFIRMED("Confirmed", CREATED),
+    DELIVERED("Delivered", CONFIRMED),
+    CANCELED("Canceled", CREATED);
 
     private String description;
+    private List<OrderStatus> statusOlds;
 
-    OrderStatus(String description) {
+    OrderStatus(String description, OrderStatus... statusOlds) {
         this.description = description;
+        this.statusOlds = Arrays.asList(statusOlds);
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public boolean cannotChangeTo(OrderStatus newStatus) {
+        return !newStatus.statusOlds.contains(this);
     }
 }

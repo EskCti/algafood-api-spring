1cecd2c347462bdb112c35fa6301766368ae5d3e
eskokado <eskokado@gmail.com>
2023-07-13 10:15:06 -0300

    fix: update long id to String code into OrderNotFoundException

--- src/main/java/com/eskcti/algafoodapi/domain/exceptions/OrderNotFoundException.java
+++ src/main/java/com/eskcti/algafoodapi/domain/exceptions/OrderNotFoundException.java
@@ -3,11 +3,7 @@ package com.eskcti.algafoodapi.domain.exceptions;
 public class OrderNotFoundException extends EntityNotFoundException {
     private static final long serialVersionUID = 1l;
 
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-
-    public OrderNotFoundException(Long orderId) {
-        this(String.format("Not found Order with id %d", orderId));
+    public OrderNotFoundException(String orderCode) {
+        super(String.format("Not found Order with code %s", orderCode));
     }
 }

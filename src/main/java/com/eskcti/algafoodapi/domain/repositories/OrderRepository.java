bc6514e26a2b2f3ea1c26f737daf46106712bb49
eskokado <eskokado@gmail.com>
2023-07-13 10:15:27 -0300

    fix: update long id to String code into OrderRepository

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/OrderRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/OrderRepository.java
@@ -4,8 +4,10 @@ import com.eskcti.algafoodapi.domain.models.Order;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends CustomJpaRepository<Order, Long> {
+    Optional<Order> findByCode(String code);
     @Query("from Order o join fetch o.customer c join fetch o.restaurant r join fetch r.kitchen")
     List<Order> findAlll();
 }

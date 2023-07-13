0727f1a4c66455d8d713962ec6db61ae6eaf5b5f
eskokado <eskokado@gmail.com>
2023-07-13 08:53:42 -0300

    refactor: fix endpoint confirm, delivery and cancel into flow order controller

--- src/main/java/com/eskcti/algafoodapi/domain/services/FlowOrderService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/FlowOrderService.java
@@ -17,53 +17,17 @@ public class FlowOrderService {
     @Transactional
     public void confirm(Long orderId) {
         Order order = issuanceOrderService.findById(orderId);
-
-        if (!order.getOrderStatus().equals(OrderStatus.CREATED)) {
-            throw new BusinessException(
-                    String.format("Order status %d cannot be changed from %s to %s",
-                            order.getId(),
-                            order.getOrderStatus().getDescription(),
-                            OrderStatus.CONFIRMED.getDescription()
-                    )
-            );
-        }
-
-        order.setOrderStatus(OrderStatus.CONFIRMED);
-        order.setConfirmationDate(OffsetDateTime.now());
+        order.confirm();
     }
 
     @Transactional
     public void delivery(Long orderId) {
         Order order = issuanceOrderService.findById(orderId);
-
-        if (!order.getOrderStatus().equals(OrderStatus.CONFIRMED)) {
-            throw new BusinessException(
-                    String.format("Order status %d cannot be changed from %s to %s",
-                            order.getId(),
-                            order.getOrderStatus().getDescription(),
-                            OrderStatus.DELIVERED.getDescription()
-                    )
-            );
-        }
-
-        order.setOrderStatus(OrderStatus.DELIVERED);
-        order.setConfirmationDate(OffsetDateTime.now());
+        order.delivery();
     }
     @Transactional
     public void cancel(Long orderId) {
         Order order = issuanceOrderService.findById(orderId);
-
-        if (!order.getOrderStatus().equals(OrderStatus.CREATED)) {
-            throw new BusinessException(
-                    String.format("Order status %d cannot be changed from %s to %s",
-                            order.getId(),
-                            order.getOrderStatus().getDescription(),
-                            OrderStatus.CANCELED.getDescription()
-                    )
-            );
-        }
-
-        order.setOrderStatus(OrderStatus.CANCELED);
-        order.setConfirmationDate(OffsetDateTime.now());
+        order.cancel();
     }
 }

8a68ccff18af3a5d1240830dbcce7565eb92e0ff
eskokado <eskokado@gmail.com>
2023-07-30 06:36:01 -0300

    chore: modify status one product inactive

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -56,7 +56,7 @@ INSERT IGNORE INTO tab_permissions (id, name, description) VALUES (2, "EDIT_KITC
 INSERT IGNORE INTO tab_restaurants_payments_types (restaurant_id, payment_type_id) VALUES (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3), (4, 1), (4, 2), (4, 3);
 
 INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 1.01', 'Description product 01', 15, true, 1);
-INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 1.02', 'Description product 02', 10, true, 1);
+INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 1.02', 'Description product 02', 10, false, 1);
 INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 1.03', 'Description product 03', 25, true, 1);
 INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 2.01', 'Description product 01', 15, true, 2);
 INSERT IGNORE INTO tab_products (name, description, price, active, restaurant_id) VALUES ('Product 2.02', 'Description product 02', 10, true, 2);

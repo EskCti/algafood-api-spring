8a8f3dd856fdaa07f9350480960cc749c7342c9b
eskokado <eskokado@gmail.com>
2023-08-02 05:39:54 -0300

    feat: create class DailySalesFilter

---
+++ src/main/java/com/eskcti/algafoodapi/domain/filter/DailySalesFilter.java
@@ -0,0 +1,20 @@
+package com.eskcti.algafoodapi.domain.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.OffsetDateTime;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
+
+@Getter
+@Setter
+public class DailySalesFilter {
+    private Long restaurantId;
+
+    @DateTimeFormat(iso = DATE_TIME)
+    private OffsetDateTime dateFrom;
+    @DateTimeFormat(iso = DATE_TIME)
+    private OffsetDateTime dateTo;
+}

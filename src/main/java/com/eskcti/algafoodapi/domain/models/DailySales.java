f8eaae6926305faba7f59cefd581317e5b92ff88
eskokado <eskokado@gmail.com>
2023-08-02 06:57:55 -0300

    fix: alter type date to LocalDate into DailySales

--- src/main/java/com/eskcti/algafoodapi/domain/models/DailySales.java
+++ src/main/java/com/eskcti/algafoodapi/domain/models/DailySales.java
@@ -3,18 +3,18 @@ package com.eskcti.algafoodapi.domain.models;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Setter
 @Data
 public class DailySales {
-    private Date date;
+    private LocalDate date;
     private Long totalSales;
     private BigDecimal totalBilled;
 
-    public DailySales(Date date, Long totalSales, BigDecimal totalBilled) {
+    public DailySales(LocalDate date, Long totalSales, BigDecimal totalBilled) {
         this.date = date;
         this.totalSales = totalSales;
         this.totalBilled = totalBilled;

ff270b478381353f8f7b678b4f27dabe1d98cd81
eskokado <eskokado@gmail.com>
2023-08-21 09:48:32 -0300

    fix: extends SmtpSendEmailService into FakeSendEmail Service

--- src/main/java/com/eskcti/algafoodapi/infrastruct/services/email/FakeSendEmailService.java
+++ src/main/java/com/eskcti/algafoodapi/infrastruct/services/email/FakeSendEmailService.java
@@ -11,22 +11,11 @@ import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import java.io.IOException;
 
 @Slf4j
-public class FakeSendEmailService implements SendEmailService {
-    @Autowired
-    private Configuration freeMarkerConfig;
+public class FakeSendEmailService extends SmtpSendEmailService {
     @Override
     public void send(Message message) {
         String corpo = processTemplate(message);
 
         log.info("[FAKE E-MAIL] Para: {}\n{}", message.getRecipients(), corpo);
-    }
-
-    protected String processTemplate(Message message) {
-        try {
-            Template template = freeMarkerConfig.getTemplate(message.getBody());
-            return FreeMarkerTemplateUtils.processTemplateIntoString(template, message.getVariables());
-        } catch (IOException | TemplateException e) {
-            throw new EmailException("Unable to mount template of the email", e);
-        }
     }
 }

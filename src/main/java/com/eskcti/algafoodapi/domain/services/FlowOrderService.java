db012e7b3cb61b1e5dd5f786bc8df7076cb05325
eskokado <eskokado@gmail.com>
2023-08-22 06:24:35 -0300

    fix: remove send email from confirm order into FlowOrderService

--- src/main/java/com/eskcti/algafoodapi/domain/services/FlowOrderService.java
+++ src/main/java/com/eskcti/algafoodapi/domain/services/FlowOrderService.java
@@ -1,6 +1,7 @@
 package com.eskcti.algafoodapi.domain.services;
 
 import com.eskcti.algafoodapi.domain.models.Order;
+import com.eskcti.algafoodapi.domain.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,22 +11,14 @@ public class FlowOrderService {
     @Autowired
     private IssuanceOrderService issuanceOrderService;
     @Autowired
-    private SendEmailService sendEmailService;
+    private OrderRepository orderRepository;
 
     @Transactional
     public void confirm(String orderCode) {
         Order order = issuanceOrderService.findByCode(orderCode);
         order.confirm();
 
-        var message = SendEmailService.Message.builder()
-                .subject(order.getRestaurant().getName() + " - Pedido confirmado")
-                .recipient(order.getCustomer().getEmail())
-                .variable("order", order)
-                .body("confirmed-order.htm")
-                .build();
-
-
-        sendEmailService.send(message);
+        orderRepository.save(order);
     }
 
     @Transactional

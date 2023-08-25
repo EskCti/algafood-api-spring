c497ce99bdded8d7da2b5ffda7c5f9e0581d4a4d
eskokado <eskokado@gmail.com>
2023-08-25 07:20:36 -0300

    feat: add getUpdatedAt into PaymentTypeRepository

--- src/main/java/com/eskcti/algafoodapi/domain/repositories/PaymentTypeRepository.java
+++ src/main/java/com/eskcti/algafoodapi/domain/repositories/PaymentTypeRepository.java
@@ -2,10 +2,14 @@ package com.eskcti.algafoodapi.domain.repositories;
 
 import com.eskcti.algafoodapi.domain.models.PaymentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Repository
 public interface PaymentTypeRepository extends JpaRepository<PaymentType, Long> {
+    @Query("select max(updatedAt) from PaymentType")
+    OffsetDateTime getUpdatedAt();
 }

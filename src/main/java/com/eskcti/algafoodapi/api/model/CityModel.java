d53e6cd9640530db00b8ad7c56e9912cbc254b47
eskokado <eskokado@gmail.com>
2023-09-05 06:19:43 -0300

    feat: add representation HAL into StateModel and CityModel

--- src/main/java/com/eskcti/algafoodapi/api/model/CityModel.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/CityModel.java
@@ -3,11 +3,12 @@ package com.eskcti.algafoodapi.api.model;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Schema(description = "Representa uma cidade")
 @Getter
 @Setter
-public class CityModel {
+public class CityModel extends RepresentationModel<CityModel> {
     @Schema(description = "ID da cidade", example = "1")
     private Long id;
 

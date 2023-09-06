7e89819318272626b3ef5ad52714a8c99c20e0c8
eskokado <eskokado@gmail.com>
2023-09-06 09:22:02 -0300

    feat: extends RepresentationModel into GroupModel and UserModel

--- src/main/java/com/eskcti/algafoodapi/api/model/GroupModel.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/GroupModel.java
@@ -2,10 +2,11 @@ package com.eskcti.algafoodapi.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
-public class GroupModel {
+public class GroupModel extends RepresentationModel<GroupModel> {
     private Long id;
     private String name;
 }

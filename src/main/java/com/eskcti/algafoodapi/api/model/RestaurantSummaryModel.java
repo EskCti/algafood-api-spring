bd16a6e2c8da6cd09fb187dcdaeeb2b5b0a8becb
eskokado <eskokado@gmail.com>
2023-09-07 10:41:26 -0300

    fix: extends RestaurantSummaryModelModel with RepresentationModel

--- src/main/java/com/eskcti/algafoodapi/api/model/RestaurantSummaryModel.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/RestaurantSummaryModel.java
@@ -2,12 +2,15 @@ package com.eskcti.algafoodapi.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.math.BigDecimal;
 
+@Relation(collectionRelation = "restaurants")
 @Getter
 @Setter
-public class RestaurantSummaryModel {
+public class RestaurantSummaryModel extends RepresentationModel<RestaurantSummaryModel> {
     private Long id;
     private String name;
 }

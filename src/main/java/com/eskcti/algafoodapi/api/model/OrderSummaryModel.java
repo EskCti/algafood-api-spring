88db61201f0aff1956cde14955baada464a90662
eskokado <eskokado@gmail.com>
2023-09-07 10:40:12 -0300

    fix: extends OrderSummaryModel with RepresentationModel

--- src/main/java/com/eskcti/algafoodapi/api/model/OrderSummaryModel.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/OrderSummaryModel.java
@@ -2,19 +2,22 @@ package com.eskcti.algafoodapi.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Relation(collectionRelation = "orders")
 @Getter
 @Setter
-public class OrderSummaryModel {
+public class OrderSummaryModel extends RepresentationModel<OrderSummaryModel> {
     private String code;
     private BigDecimal subtotal;
     private BigDecimal shippingFee;
     private BigDecimal valueTotal;
     private OffsetDateTime createdAt;
     private RestaurantSummaryModel restaurant;
-//    private UserModel customer;
+    private UserModel customer;
     private String nameCustomer;
 }

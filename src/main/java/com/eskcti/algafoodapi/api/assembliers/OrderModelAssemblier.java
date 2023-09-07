71fb18741cd98183d3acea9a9f03ebbaaf0dac35
eskokado <eskokado@gmail.com>
2023-09-07 10:42:44 -0300

    fix: extends OrderModelAssemblier with RepresentationModelaAssemblerSupport

--- src/main/java/com/eskcti/algafoodapi/api/assembliers/OrderModelAssemblier.java
+++ src/main/java/com/eskcti/algafoodapi/api/assembliers/OrderModelAssemblier.java
@@ -1,28 +1,59 @@
 package com.eskcti.algafoodapi.api.assembliers;
 
-import com.eskcti.algafoodapi.api.model.CityModel;
 import com.eskcti.algafoodapi.api.model.OrderModel;
-import com.eskcti.algafoodapi.domain.models.City;
+import com.eskcti.algafoodapi.api.resources.*;
 import com.eskcti.algafoodapi.domain.models.Order;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class OrderModelAssemblier {
+public class OrderModelAssemblier extends RepresentationModelAssemblerSupport<Order, OrderModel> {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public OrderModelAssemblier() {
+        super(OrderController.class, OrderModel.class);
+    }
     public OrderModel toModel(Order order) {
-        return modelMapper.map(order, OrderModel.class);
+        OrderModel orderModel = createModelWithId(order.getId(), order);
+        modelMapper.map(order, orderModel);
+
+        orderModel.add(linkTo(OrderController.class)
+                .withRel("orders"));
+        orderModel.getRestaurant().add(linkTo(methodOn(RestaurantController.class)
+                .find(order.getRestaurant().getId())).withSelfRel());
+        orderModel.getCustomer().add(linkTo(methodOn(UserController.class)
+                .find(order.getCustomer().getId())).withSelfRel());
+        orderModel.getPaymentType().add(linkTo(methodOn(PaymentTypeController.class)
+                .find(null, order.getPaymentType().getId())).withSelfRel());
+        orderModel.getAddressDelivery().getCity().add(linkTo(methodOn(CityController.class)
+                .find(order.getAddressDelivery().getCity().getId())).withSelfRel());
+
+        orderModel.getItems().forEach(item -> {
+            item.add(linkTo(methodOn(RestaurantProductController.class)
+                    .find(orderModel.getRestaurant().getId(), item.getProductId()))
+                    .withRel("product"));
+        });
+
+        return orderModel;
     }
 
-    public List<OrderModel> toCollectionModel(List<Order> orders) {
-        return orders.stream()
-                .map(order -> toModel(order))
-                .collect(Collectors.toList());
+    @Override
+    public CollectionModel<OrderModel> toCollectionModel(Iterable<? extends Order> entities) {
+        return super.toCollectionModel(entities)
+                .add(linkTo(OrderController.class).withSelfRel());
     }
+
+//    public List<OrderModel> toCollectionModel(List<Order> orders) {
+//        return orders.stream()
+//                .map(order -> toModel(order))
+//                .collect(Collectors.toList());
+//    }
 }

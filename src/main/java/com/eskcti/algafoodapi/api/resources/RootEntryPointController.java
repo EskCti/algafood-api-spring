e70ee0b288146f9f2da7b354db2973f53de81549
eskokado <eskokado@gmail.com>
2023-10-22 07:09:16 -0300

    feat: create root entry point controller

---
+++ src/main/java/com/eskcti/algafoodapi/api/resources/RootEntryPointController.java
@@ -0,0 +1,36 @@
+package com.eskcti.algafoodapi.api.resources;
+
+import com.eskcti.algafoodapi.api.AlgaLinks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+    @Autowired
+    private AlgaLinks algaLinks;
+
+    @GetMapping
+    public RootEntryPointModel root() {
+        var rootEntryPointModel = new RootEntryPointModel();
+
+        rootEntryPointModel.add(algaLinks.linkToKitchens());
+        rootEntryPointModel.add(algaLinks.linkToOrders());
+        rootEntryPointModel.add(algaLinks.linkToRestaurants());
+        rootEntryPointModel.add(algaLinks.linkToGroups("groups"));
+        rootEntryPointModel.add(algaLinks.linkToUsers());
+        rootEntryPointModel.add(algaLinks.linkToPaymentTypes());
+        rootEntryPointModel.add(algaLinks.linkToStates());
+        rootEntryPointModel.add(algaLinks.linkToStates());
+        rootEntryPointModel.add(algaLinks.linkToCities());
+
+        return rootEntryPointModel;
+    }
+    private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+
+    }
+}

eb1418ca7963154bbec53d4af621790c0e8c3b3f
eskokado <eskokado@gmail.com>
2023-10-29 13:58:08 -0300

    feat: describing restaurant endpoint documentation

--- src/main/java/com/eskcti/algafoodapi/api/model/RestaurantModel.java
+++ src/main/java/com/eskcti/algafoodapi/api/model/RestaurantModel.java
@@ -2,6 +2,7 @@ package com.eskcti.algafoodapi.api.model;
 
 import com.eskcti.algafoodapi.api.model.view.RestaurantView;
 import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -13,12 +14,15 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class RestaurantModel extends RepresentationModel<RestaurantModel> {
+    @Schema(example = "1")
     @JsonView(RestaurantView.Summary.class)
     private Long id;
 
+    @Schema(example = "Thai Gourmet")
     @JsonView(RestaurantView.Summary.class)
     private String name;
 
+    @Schema(example = "12.00")
     @JsonView(RestaurantView.Summary.class)
     private BigDecimal shippingFee;
 
--- src/main/java/com/eskcti/algafoodapi/api/resources/openapi/RestaurantControllerOpenApi.java
+++ src/main/java/com/eskcti/algafoodapi/api/resources/openapi/RestaurantControllerOpenApi.java
@@ -6,6 +6,9 @@ import com.eskcti.algafoodapi.domain.models.Restaurant;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -21,7 +24,7 @@ import java.util.Map;
 @Tag(name = "Restaurantes", description = "Gerencia os restaurantes")
 public interface RestaurantControllerOpenApi {
 
-    @Operation(parameters = {
+    @Operation(summary = "Lista restaurantes", parameters = {
         @Parameter(name = "project",
             description = "Resumo da projeção",
             example = "summary",
@@ -31,20 +34,74 @@ public interface RestaurantControllerOpenApi {
     })
     CollectionModel<RestaurantModel> listSummary();
     MappingJacksonValue listMapping();
-    RestaurantModel find(@PathVariable Long id);
+
+    @Operation(summary = "Busca um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "ID do restaurante inválido", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
+    RestaurantModel find(
+            @Parameter(description = "ID de um restaurante", example = "1", required = true)
+            @PathVariable Long id);
+
+    @Operation(summary = "Cadastra um restaurante", responses = {
+            @ApiResponse(responseCode = "201", description = "Restaurante cadastrado"),
+    })
     RestaurantModel insert(@RequestBody @Valid RestaurantInput restaurantInput);
+
+    @Operation(summary = "Atualiza um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "200", description = "Restaurante atualizado"),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
     RestaurantModel update(@PathVariable Long id, @RequestBody @Valid RestaurantInput restaurantInput);
+
+    @Operation(summary = "Excluir um restaurante por ID")
     void remove(@PathVariable Long id);
     RestaurantModel updatePartial(
             @PathVariable Long restaurantId,
             @RequestBody Map<String, Object> fields,
             HttpServletRequest request
     );
+
+    @Operation(summary = "Ativa um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurante ativado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
     ResponseEntity<Void> activate (@PathVariable Long restaurantId);
+
+    @Operation(summary = "Desativa um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurante inativado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
     ResponseEntity<Void> deactivate (@PathVariable Long restaurantId);
+
+    @Operation(summary = "Ativa múltiplos restaurantes", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurantes ativados com sucesso"),
+    })
     void activateMultiples(@RequestBody List<Long> restaurantsIds);
+
+    @Operation(summary = "Inativa múltiplos restaurantes", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurantes ativados com sucesso"),
+    })
     void deactivateMultiples(@RequestBody List<Long> restaurantsIds);
+
+    @Operation(summary = "Abre um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurante aberto com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
     ResponseEntity<Void> opening (@PathVariable Long restaurantId);
+
+    @Operation(summary = "Fecha um restaurante por ID", responses = {
+            @ApiResponse(responseCode = "204", description = "Restaurante fechado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+                    @Content(schema = @Schema(ref = "Problema")) }),
+    })
     ResponseEntity<Void> closing (@PathVariable Long restaurantId);
     void validate(Restaurant restaurant, String objectName);
     void merge(Map<String, Object> fieldsSource, Restaurant restaurantTarget, HttpServletRequest request);

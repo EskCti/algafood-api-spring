6654acf158a7b19abe164c5a3b55046fabf475bc
eskokado <eskokado@gmail.com>
2023-12-03 08:45:16 -0300

    refactor: fix filter chain

--- src/main/java/com/eskcti/algafoodapi/core/security/WebSecurityConfig.java
+++ src/main/java/com/eskcti/algafoodapi/core/security/WebSecurityConfig.java
@@ -2,69 +2,69 @@ package com.eskcti.algafoodapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = bCryptPasswordEncoder();
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("edson")
-                .password(bCryptPasswordEncoder.encode("123"))
-                .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin")
-                .password(bCryptPasswordEncoder.encode("123"))
-                .roles("ADMIN")
-                .build());
-        return manager;
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(
-            HttpSecurity http,
-            BCryptPasswordEncoder bCryptPasswordEncoder,
-            UserDetailsService userDetailService
-    ) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailService)
-                .passwordEncoder(bCryptPasswordEncoder)
-                .and()
-                .build();
-    }
-
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        BCryptPasswordEncoder bCryptPasswordEncoder = bCryptPasswordEncoder();
+//
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("edson")
+//                .password(bCryptPasswordEncoder.encode("123"))
+//                .roles("USER")
+//                .build());
+//        manager.createUser(User.withUsername("admin")
+//                .password(bCryptPasswordEncoder.encode("123"))
+//                .roles("ADMIN")
+//                .build());
+//        return manager;
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            HttpSecurity http,
+//            BCryptPasswordEncoder bCryptPasswordEncoder,
+//            UserDetailsService userDetailService
+//    ) throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(userDetailService)
+//                .passwordEncoder(bCryptPasswordEncoder)
+//                .and()
+//                .build();
+//    }
+//
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/v1/states/**")
-                        .hasAnyRole("ADMIN")
-                        .requestMatchers("/v1/cities/**")
-                        .hasAnyRole("ADMIN")
                         .anyRequest().authenticated())
-                .httpBasic()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .cors().and()
+                .oauth2ResourceServer().jwt();
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("5MU8/YgrnHELFHEypwoIfstdqvzxUE4KoZe1vpG+6S8=".getBytes(), "HmacSHA256");
+
+        JwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(secretKey).build();
+
+        return jwtDecoder;
     }
 }
 

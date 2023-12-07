07cfd796158ff52bdb51433ccca05498a73fdbc0
eskokado <eskokado@gmail.com>
2023-12-07 05:37:57 -0300

    refactor: rename class WebSecurityConfig to ResourceServerConfig

--- src/main/java/com/eskcti/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/eskcti/algafoodapi/core/security/ResourceServerConfig.java
@@ -2,7 +2,6 @@ package com.eskcti.algafoodapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +15,7 @@ import java.util.stream.Collectors;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
+public class ResourceServerConfig {
 
 //    @Bean
 //    public BCryptPasswordEncoder bCryptPasswordEncoder() {

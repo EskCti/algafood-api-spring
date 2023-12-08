393256a62612c6155ac196d49df3c58e1550cf7a
eskokado <eskokado@gmail.com>
2023-12-08 09:04:33 -0300

    feat: create managerOrder into AlgaSecurity

--- src/main/java/com/eskcti/algafoodapi/core/security/AlgaSecurity.java
+++ src/main/java/com/eskcti/algafoodapi/core/security/AlgaSecurity.java
@@ -1,5 +1,6 @@
 package com.eskcti.algafoodapi.core.security;
 
+import com.eskcti.algafoodapi.domain.repositories.OrderRepository;
 import com.eskcti.algafoodapi.domain.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,9 @@ public class AlgaSecurity {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
@@ -29,5 +33,14 @@ public class AlgaSecurity {
         var existsResponsible = restaurantRepository.existsResponsible(restaurantId, getUserId());
         System.out.println("*************" + existsResponsible + "******************");
         return existsResponsible;
+    }
+
+    public boolean managerOrder(String orderCode) {
+        if (orderCode == null) {
+            return false;
+        }
+        var orderManagerBy = orderRepository.isManagerBy(orderCode, getUserId());
+        System.out.println("*************" + orderManagerBy + "******************");
+        return orderManagerBy;
     }
 }

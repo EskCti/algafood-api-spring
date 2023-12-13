c77d0d0cfbc55b84336088808cd3de08eed0291b
eskokado <eskokado@gmail.com>
2023-12-13 05:48:53 -0300

    fix: fix jksLocation

--- src/main/java/com/eskcti/algafoodapi/core/security/authorizationserver/JwtKeyStoreProperties.java
+++ src/main/java/com/eskcti/algafoodapi/core/security/authorizationserver/JwtKeyStoreProperties.java
@@ -1,41 +1,22 @@
 package com.eskcti.algafoodapi.core.security.authorizationserver;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+@Getter
+@Setter
 @Validated
 @Component
 @ConfigurationProperties("algafood.jwt.keystore")
 public class JwtKeyStoreProperties {
 
-    private String path;
-
     private String password;
 
     private String keypairAlias;
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getKeypairAlias() {
-        return keypairAlias;
-    }
-
-    public void setKeypairAlias(String keypairAlias) {
-        this.keypairAlias = keypairAlias;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
+    private Resource jksLocation;
 }

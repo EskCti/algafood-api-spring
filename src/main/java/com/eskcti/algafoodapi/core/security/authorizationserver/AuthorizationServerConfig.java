40c0e6563dd9502b0573138b91687dc880e8b56d
eskokado <eskokado@gmail.com>
2023-12-13 05:49:51 -0300

    feat: add JWSSource and JWKSet and RSAKey

--- src/main/java/com/eskcti/algafoodapi/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/eskcti/algafoodapi/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -1,9 +1,16 @@
 package com.eskcti.algafoodapi.core.security.authorizationserver;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,11 +27,15 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.time.Duration;
 
 @Configuration
 public class AuthorizationServerConfig {
 
+    @Autowired
+    private JwtKeyStoreProperties jwtKeyStoreProperties;
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
@@ -50,7 +61,7 @@ public class AuthorizationServerConfig {
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
                 .scope("READ")
                 .tokenSettings(TokenSettings.builder()
-                        .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                        .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
                         .accessTokenTimeToLive(Duration.ofMinutes(30)).build())
                 .build();
 
@@ -64,5 +75,34 @@ public class AuthorizationServerConfig {
                 jdbcOperations,
                 registeredClientRepository
         );
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(JwtKeyStoreProperties properties) throws Exception {
+        var rsaKey = rsaKey();
+
+        return new ImmutableJWKSet<>(new JWKSet(rsaKey));
+    }
+
+    @Bean
+    public JWKSet jwkSet() throws Exception{
+        var rsakey = rsaKey();
+
+        return new JWKSet(rsakey);
+    }
+
+    private RSAKey rsaKey() throws Exception {
+        char[] keyStorePass = jwtKeyStoreProperties.getPassword().toCharArray();
+        String keypairAlias = jwtKeyStoreProperties.getKeypairAlias();
+
+        Resource jksLocation = jwtKeyStoreProperties.getJksLocation();
+        InputStream inputStream = jksLocation.getInputStream();
+
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(inputStream, keyStorePass);
+
+        RSAKey rsaKey = RSAKey.load(keyStore, keypairAlias, keyStorePass);
+
+        return rsaKey;
     }
 }

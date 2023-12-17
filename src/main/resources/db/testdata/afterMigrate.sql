d274ff7b123f978c64bd58e20d3a32a93bdfafad
eskokado <eskokado@gmail.com>
2023-12-17 09:14:40 -0300

    chore: add lock tables write

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,3 +1,25 @@
+set foreign_key_checks = 0;
+
+lock tables
+    tab_restaurants_responsible write,
+    tab_restaurants_payments_types write,
+    tab_groups_permissions write,
+    tab_users_groups write,
+    tab_orders_items write,
+    tab_orders write,
+    tab_products_photos write,
+    tab_products write,
+    tab_payments_type write,
+    tab_permissions write,
+    tab_restaurants write,
+    tab_cities write,
+    tab_states write,
+    tab_kitchens write,
+    tab_groups write,
+    tab_users write,
+    oauth2_authorization write,
+    oauth2_registered_client write;
+
 DELETE FROM tab_restaurants_responsible;
 DELETE FROM tab_restaurants_payments_types;
 DELETE FROM tab_groups_permissions;
@@ -205,3 +227,5 @@ VALUES('2', 'algafood-web', '2023-12-14 09:16:38', '$2a$10$8sAVr8Vj/b8R18lvbozAb
 INSERT INTO oauth2_registered_client
 (id, client_id, client_id_issued_at, client_secret, client_secret_expires_at, client_name, client_authentication_methods, authorization_grant_types, redirect_uris, scopes, client_settings, token_settings)
 VALUES('3', 'foodanalytics', '2023-12-14 09:16:38', '$2a$10$lIeKhXRllilZNjYNZqBhqOgsFJvi1i3f.yJAqjch7DDzQJ4eBp5qa', NULL, 'Food Analytics', 'client_secret_basic', 'authorization_code', 'http://www.foodanalytics.local:8082', 'READ,WRITE', '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}', '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000]}');
+
+unlock tables;
